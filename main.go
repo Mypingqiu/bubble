658e3d777e424b0c90d912d2cf17fde5394b76ca
mypingqiu <18338456560@163.com>
2022-07-04 18:50:22 +0800

    编写简单web执行程序 --v1.0.1

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//创建默认的路由引擎
+	r := gin.Default()
+	r.GET("/bubble", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "测试记事本",
+		})
+	})
+	r.Run(":8080")
+}
