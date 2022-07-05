8f6244da80e3925f8bb40aa5e80a138369d7fd49
mypingqiu <18338456560@163.com>
2022-07-05 11:15:36 +0800

    添加静态文件、html文件 --v1.0.1

--- main.go
+++ main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	//创建默认的路由引擎
 	r := gin.Default()
-	r.GET("/bubble", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "测试记事本",
-		})
+	//告诉gin框架模版文件引用的静态文件
+	r.Static("/static", "static")
+	// 告诉gin框架去哪里找模版文件
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 	//测试
 	r.Run(":8080")
